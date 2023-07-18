d808181adb3455f369a608741b955c6c6612a21d
Benjamin-eBrainer <515ebrainer@gmail.com>
2023-07-18 15:26:50 +0300

    Completed the Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Amazon Clone</h1>
+      <Header />
       <Outlet />
     </div>
   );
