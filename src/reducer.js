8689e4472ea3877d9e183e3b63d5510fb38a1794
Benjamin-eBrainer <515ebrainer@gmail.com>
2023-07-22 11:35:10 +0300

    Built the subtotal component

--- src/reducer.js
+++ src/reducer.js
@@ -1,33 +1,45 @@
 export const initialState = {
-    basket: [],
-    user: null
-}
+  basket: [],
+  user: null,
+};
+
+export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0);
 
 function reducer(state, action) {
-    switch (action.type) {
-        case 'ADD_TO_BASKET':
-            return {
-                ...state,
-                basket: [...state.basket, action.payload]
-            }
-            break;
-        case 'REMOVE_FROM_BASKET':
-            return {
-                ...state,
-                basket: state.basket.filter(item => item.id !== action.payload)
-            }
-            break;
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.payload],
+      };
+      break;
+    case "REMOVE_FROM_BASKET":
+      let newBasket = [...state.basket];
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.id} as its not in the basket)`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
 
-        case 'SET_USER':
-            return {
-                ...state,
-                user: action.payload
-            }
-            break;
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
+      break;
 
-        default:
-            return state
-    }
+    default:
+      return state;
+  }
 }
 
 export default reducer;
